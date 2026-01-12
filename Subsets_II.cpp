d3f99c4dd03a3ab0d8fd2f32c827cb0ce8cee312
Krishna Sharma <rakesh7355249204@gmail.com>
2026-01-12 16:49:38 +0530

    Subsets_II

--- Subsets_II.cpp
+++ Subsets_II.cpp
@@ -2,45 +2,47 @@
 #include <vector>
 #include <set>
 using namespace std;
-    set<vector<int> >subset;
-    vector<vector<int>> valids;
-    void generate(vector<int> &sub,int i,vector<int> &nums){
-        if(i==nums.size()){
-            subset.insert(sub);
-            return ;
-        }
-        //ith element is not taken : 
-        generate(sub,i+1,nums);
 
-        //ith element is now taken :
-        sub.push_back(nums[i]);
-        generate(sub,i+1,nums);
-        sub.pop_back();
+void putUniqueSubsets(set<vector<int> > &ds,int num,vector<int> &nums){
+
+    int j = 0;
+
+    vector<int> temp;
+    while(num > 0){
+        if(num & 1) temp.push_back(nums[j]);
+
+        j++;
+        num >>= 1;
+    }
+    sort(temp.begin(),temp.end());
+    ds.insert(temp);
+}
+
+vector<vector<int>> subsetsWithDup(vector<int>& nums){
+    int n = nums.size();
+    set<vector<int>> ds;
+
+    for(int i = 0;i < (1<<n) ; ++i){
+        putUniqueSubsets(ds,i,nums);
     }
 
-    int main(){
-        int n;
-        cin >> n;
-        vector<int> nums(n);
-        for(int i=0;i<n;++i){
-            cin >> nums[i];
-        }
-        vector<int> empty;
-        generate(empty,0,nums);
-        // for(auto ele: subset){
-        //     for(auto it : ele){
-        //         cout << it << " ";
-        //     }
-        //     cout << endl;
-        // }
-        for(auto ele : subset){
-            valids.push_back(ele);
-        }
-        for(auto ele : valids){
-            for(auto it : ele){
-                cout << it << " ";
-            }
-            cout << endl;
-        }
-        return 0;
+    return vector<vector<int>>(ds.begin(), ds.end());
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i=0;i<n;++i){
+        cin >> nums[i];
     }
+    vector<vector<int> > ans = subsetsWithDup(nums);
+
+    for(auto x : ans){
+        for(auto ele : x){
+            cout << ele << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
